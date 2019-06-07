a9287fcda2147b5f4f0988ef7203b73f07cbb556
Tin Hoang <tin.hoang@ama.ab.ca>
2019-06-07 11:07:12 -0600

    remove test

--- src/Coverage.tsx
+++ src/Coverage.tsx
@@ -1,11 +1,23 @@
+
 import React, { Component } from "react";
 
+/* istanbul ignore next */
 function Coverage() {
   return (
     <div>
       <span>Hello World!</span>    
      </div>
     );
+}
+
+
+function CoverageIgnore(){
+  return (
+    <div>
+      <span>Yo</span>
+    </div>
+  )
+
 }
 
 export default Coverage;
